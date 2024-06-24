4ad6501d82bf6257dd348a1e4d21b61155952be5
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-06-24 22:00:09 +0530

    small error

---
+++ matplotlib.py
@@ -0,0 +1,14 @@
+# import matplotlib.pyplot as plt
+# a=['Jan','feb','Mar','Apr','May','Jun']
+# b= [1200,400,3400,2300,1200,4500]
+# plt.title = 'Copy Sale'
+# plt.xlabel = 'Months'
+# plt.ylabel = 'Sales'
+# plt.plot(a,b)
+# try:
+#     import matplotlib
+#     print("Matplotlib is installed.")
+# except ImportError:
+#     print("Matplotlib is not installed.")
+
+import matplotlib.pyplot as plt
